00b6fd85d08504f2b6790ead3f8e680297c9a769
Jaeseok Huh <iriszero5@gmail.com>
2017-09-17 13:44:10 +0900

    Clarify the condition

--- src/NetworkFlow.cpp
+++ src/NetworkFlow.cpp
@@ -2,4 +2,5 @@
  * for each edge a->b whose capacity is [l, r]
  * 1) a->b with capacity l, cost -1 and with capacity r-l, cost 0
  * 2) new source -> b with capacity l, a -> new sink with capacity l, a->b with capacity r-l, sink->source with capacity INF
+ * and check that the Maximum Flow is eqaul to l
  */
