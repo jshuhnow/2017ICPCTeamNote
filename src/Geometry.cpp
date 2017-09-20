44c716d179accdb00ea0388d97cef591bf2f3055
김태훈 <2015004084@hanyang.ac.kr>
2017-09-20 17:35:29 +0900

    Modify the types and add convexhull

--- src/Geometry.cpp
+++ src/Geometry.cpp
@@ -1,18 +1,20 @@
+typedef long long ll;
 struct Point {
-    int x, y;
+    ll x, y;
 };
 struct Line {
     Point p1, p2;
 };
 
-int get_dist(Line l, Line r) {
+// Note that Lines are either vertical or horizontal and variable type is NOT reference
+ll get_dist(Line l, Line r) { 
     if (l.p1.x > l.p2.x) swap(l.p1, l.p2);
     if (l.p1.y > l.p2.y) swap(l.p1, l.p2);
     if (r.p1.x > r.p2.x) swap(r.p1, r.p2);
     if (r.p1.y > r.p2.y) swap(r.p1, r.p2);
     if (r.p1.x == r.p2.x) swap(l, r);
-    const int INF = 2e9;
-    int res = INF;
+    const ll INF = 1e15;
+    ll res = INF;
     if (l.p1.y == l.p2.y) {
         assert(r.p1.y == r.p2.y);
         if (!(l.p2.x < r.p1.x ||  r.p2.x < l.p1.x)) res = min(res, abs(l.p1.y - r.p1.y));
@@ -33,4 +35,33 @@ int get_dist(Line l, Line r) {
         for (auto &j : {r.p1, r.p2})
             res = min(res, (i.x - j.x) * (i.x - j.x) + (i.y-j.y) * (i.y-j.y));
     return res;
+}
+
+
+
+ll cross(const Point &O, const Point &A, const Point &B){
+	return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
+}
+
+// param:: vector of Point with x,y coordinates in long long int, P.size >= 3
+// return:: convex_hull with x, y coordinates in long long int
+// the first and the last element is SAME
+typedef long long ll;
+vector<Point> convex_hull(const vector<Point> &points)
+{
+    int k = 0;
+    vector<Point> result(2 * points.size());
+    sort(points.begin(), points.end(), [](Point p, Point q) { return p.second > q.second || ((!(p.second < q.second) && p.first < q.first)); });
+    for (int i = 0; i < points.size(); ++i) 
+    {
+        while (k >= 2 && cross(result[k - 2], result[k - 1], points[i]) <= 0)  k--;
+        result[k++] = points[i];
+    }
+    for (int i = points.size() - 2, t = k + 1; i >= 0; i--) 
+    {
+        while (k >= t && cross(result[k - 2], result[k - 1], points[i]) <= 0) k--;
+        result[k++] = points[i];
+    }
+    result.resize(k); //Circular - result[0] == result[k-1]
+    return result; 
 }
