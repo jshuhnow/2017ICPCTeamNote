a38860b1ec15b96cf6ba2afd444092d7fd52dcf0
Jaeseok Huh <iriszero5@gmail.com>
2017-09-23 00:54:46 +0900

    Rearrange code

--- src/NetworkFlow.cpp
+++ src/NetworkFlow.cpp
@@ -3,8 +3,10 @@
  * 1) a->b with capacity l, cost -1 and with capacity r-l, cost 0
  * 2) new source -> b with capacity l, a -> new sink with capacity l, a->b with capacity r-l, sink->source with capacity INF
  * and check that the Maximum Flow is eqaul to the summation of 'l's
+ *
+ * actual flow - do maxflow(oldsrc, olddst)
  */
-
+// O(V^2E) / O( min( V^(2/3)E, E^(3/2)) with UNIT capacity!
 struct Dinic {
 	typedef long long flow_t;
 	struct Edge {
@@ -70,30 +72,99 @@ struct Dinic {
 	}
 };
 
+struct EdmondKarp {
+	const int INF = 987654321;
 
-bool dfs(size_t now, const vector<vector<int>> &graph,
-	vector<bool> &visited, vector<size_t> &back_match) {
-	if (visited[now]) return false;
- 		visited[now] = true;
- 	for (int nxt: graph[now]) {
-		if (back_match[nxt] == -1 ||
-			dfs(back_match[nxt], graph, visited, back_match)) {
-			back_match[nxt] = now;
-			return true;
- 		}
- 	}
 
-	return false;
-}
-
-int bipartite_match(const vector<vector<int>> &graph) {
-	int matched = 0;
-	vector<bool> visited(graph.size(), false);
-	vector<size_t> back_match(graph.size(), -1);
-	for (size_t i =0; i<graph.size(); i++) {
-		if (dfs(i, graph, visited, back_match)) {
-			matched++;
-		}
+	int min(int a, int b) {
+	    return a<b? a:b;
 	}
-	return matched;
+	pair<int, vector<int>> BFS(const vector<vector<int>> &cap, const vector<vector<int>> &graph,
+	                           vector<vector<int>> &flow, const int src, const int sink) {
+	    vector<int> prv(graph.size(), -1);
+	    vector<int> M(graph.size(), -1);
+	    prv[src]=-2; M[src] = INF;
+
+	    queue<int> q; q.push(src);
+
+	    while(!q.empty()) {
+	        int u = q.front(); q.pop();
+	        for (int v : graph[u]) {
+	            if (cap[u][v] - flow[u][v] > 0 && prv[v] == -1) {
+	                prv[v] = u;
+	                M[v] = min(M[u], cap[u][v] - flow[u][v]);
+	                if (v!=sink) {
+	                    q.push(v);
+	                }
+	                else {
+	                    return make_pair(M[sink], prv);
+	                }
+	            }
+	        }
+	    }
+	    return make_pair(0, prv);
+	}
+
+	//Edmonds Karp Algorithm
+	int MaxFlow(const vector<vector<int>> cap, const vector<vector<int>> graph,
+	            const int src, const int sink) {
+	    int sum =0;
+	    vector<vector<int>> flow(graph.size(), vector<int>(graph.size(),0));
+
+	    while(true) {
+	        //BFS
+	        pair<int, vector<int>> ret = BFS(cap, graph, flow, src, sink);
+	        int m = ret.first; vector<int> &prv = ret.second;
+
+	        if (m == 0) break;
+	        sum+= m;
+	        
+	        int v= sink;
+	        while(v != src) {
+	            int u = prv[v];
+	            flow[u][v] += m;
+	            flow[v][u] -= m;
+	            v = u;
+	        }
+	    }
+	    return sum;
+	}
+
+	/* Usage
+	    vector<vector<int>> graph(V), cap(V, vector<int>(V, 0));
+	    graph[src].push_back(dst);
+	    graph[dst].push_back(src);
+	    cap[src][dst] = 1;
+
+	    printf("%d\n", MaxFlow(cap,graph, src, dst));
+	*/
+};
+
+struct BipartieMatch {
+	bool dfs(size_t now, const vector<vector<int>> &graph,
+		vector<bool> &visited, vector<size_t> &back_match) {
+		if (visited[now]) return false;
+			visited[now] = true;
+		for (int nxt: graph[now]) {
+			if (back_match[nxt] == -1 ||
+				dfs(back_match[nxt], graph, visited, back_match)) {
+				back_match[nxt] = now;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	int bipartite_match(const vector<vector<int>> &graph) {
+		int matched = 0;
+		vector<bool> visited(graph.size(), false);
+		vector<size_t> back_match(graph.size(), -1);
+		for (size_t i =0; i<graph.size(); i++) {
+			if (dfs(i, graph, visited, back_match)) {
+				matched++;
+			}
+		}
+		return matched;
+	}
 }
