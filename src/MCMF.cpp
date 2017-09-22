832fa85080e7983a58c2fc885666c503cc111673
Jaeseok Huh <iriszero5@gmail.com>
2017-09-23 04:30:52 +0900

    SPFA

--- src/MCMF.cpp
+++ src/MCMF.cpp
@@ -104,5 +104,26 @@ pair<cap_t, cost_t> solve(int source, int sink)
     return make_pair(total_flow, total_cost);
 }
 
+struct SPFA {
+    vi dist(n, INF); dist[S] = 0;
+    queue<int> q; q.push(S);
+    vi in_queue(n, 0); in_queue[S] = 1;
 
+    while (!q.empty()) {
+      int u = q.front(); q.pop(); in_queue[u] = 0;
+      for (j = 0; j < (int)AdjList[u].size(); j++) { // all outgoing edges from u
+        int v = AdjList[u][j].first, weight_u_v = AdjList[u][j].second;
+        if (dist[u] + weight_u_v < dist[v]) { // if can relax
+          dist[v] = dist[u] + weight_u_v; // relax
+          if (!in_queue[v]) { // add to the queue only if it's not in the queue
+            q.push(v);
+            in_queue[v] = 1;
+          }
+        }
+      }
+    }
+
+
+    //return dist
+}
 
