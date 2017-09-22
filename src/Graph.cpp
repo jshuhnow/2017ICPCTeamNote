5a34da22fdd88904ea01d6d640763cd5fcb95abf
Jaeseok Huh <iriszero5@gmail.com>
2017-09-23 00:54:17 +0900

    Hopcraft

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -37,5 +37,104 @@ void findArticulation() {
 			chk[i] = (rootChildren > 1);
 		}
 	}
+
 }
-				
+
+
+// O(E V^0.5)
+size_t q;
+
+namespace HopcroftKarp {
+	const size_t &INF = numeric_limits<size_t>::max();
+	const size_t &NIL = 0;
+
+	vector<size_t> pairL, pairR, level;
+	queue<size_t> que;
+	const vector<vector<size_t>> *graph;
+	size_t n, totalMatching;
+
+	inline bool bfs() {
+		for (size_t left = 1; left <= n; left++) {
+			if (pairL[left] == NIL) {
+				level[left] = 0;
+				que.emplace(left);
+			}
+			else level[left] = INF;
+		}
+		level[NIL] = INF;
+
+		while (que.size()) {
+			size_t left = que.front();
+			que.pop();
+
+			if (level[left] >= level[NIL]) continue;
+
+			for (size_t right : graph->at((left - 1) % q + 1)) {
+				size_t prevPair = pairR[right];
+
+				if (level[prevPair] == INF) {
+					level[prevPair] = level[left] + 1;
+					que.emplace(prevPair);
+				}
+			}
+		}
+
+		return level[NIL] != INF;
+	}
+
+	bool dfs(size_t left) {
+		if (left == NIL) return true;
+
+		for (size_t right : graph->at((left - 1) % q + 1)) {
+			size_t &traceLink = pairR[right];
+
+			if (level[traceLink] == level[left] + 1 && dfs(traceLink)) {
+				traceLink = left;
+				pairL[left] = right;
+				return true;
+			}
+		}
+
+		level[left] = INF;
+		return false;
+	}
+
+	size_t maximumMatching(const vector<vector<size_t>> &graph, size_t n, size_t m) {
+		HopcroftKarp::graph = &graph;
+		HopcroftKarp::n = n;
+
+		level.resize(n + 1);
+		pairL.resize(n + 1);
+		fill(pairL.begin(), pairL.end(), NIL);
+		pairR.resize(m + 1);
+		fill(pairR.begin(), pairR.end(), NIL);
+		totalMatching = 0;
+
+		while (bfs()) {
+			for (size_t left = 1; left <= n; left++) {
+				if (pairL[left] == NIL && dfs(left)) {
+					totalMatching++;
+				}
+			}
+		}
+
+		return totalMatching;
+	}
+}
+
+/* Usage
+	size_t n, m, p, a;
+	scanf("%zu%zu", &n, &m);
+	vector<vector<size_t>> graph(n + 1);
+
+	q = n;
+	for (size_t i = 1; i <= n; i++) {
+		scanf("%zu", &p);
+		while (p--) {
+			scanf("%zu", &a);
+			graph[i].emplace_back(a);
+		}
+	}
+
+	printf("%zu", HopcroftKarp::maximumMatching(graph, n + n, m));
+*/				
