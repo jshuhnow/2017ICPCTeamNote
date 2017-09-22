0a574e24ed3b108e80bb65d23b69fb0ce018df98
Jaeseok Huh <iriszero5@gmail.com>
2017-09-22 20:44:08 +0900

    Export as Document

--- src/ShortestPath.cpp
+++ src/ShortestPath.cpp
@@ -1,4 +1,4 @@
 // Dijkstra - O((V+E)logV) with priority queue - with an important checking - if (now_dist > dist[now_idx]) continue;
 // BelmanFord - do V-1 iteration - O(VE) with adj list. V-th iteration checks the existence of negative cycle
 // Floyd-Warshall - k, i, j O(V^3) - applicable to graph with negative edges.
-// 	Cycle Detection - init d[i][i] = INF, check whether d[i][i] >= 0 still 
+// 	Cycle Detection - init d[i][i] = INF, check whether d[i][i] >= 0 still
